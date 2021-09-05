4b5106e360b9eb775cfcd3e6c31884cc6a29099b
Максим Калядин <kalyadin.maksim@mail.ru>
2021-09-05 15:21:39 +0400

    first commit

---
+++ Labs/Lab_1.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+[<EntryPoint>]
+(*[1..100]
+|> List.map(string)
+|> List.filter(fun x -> x.Contains("1"))
+|> List.sumBy(int)
+|> printf "%i"*)
+
+["врешоу"; "риртщр"; "тррвгоовЕово"; "о"; "орщшщтттщ"; "еЕ"; "КК"; "щтщ"]
+|> List.filter(fun x -> not (x.Contains("е")))
+|> List.filter(fun x -> not (x.Contains("Е")))
+|> List.iter (fun i -> printf "%s " i)
+
+
